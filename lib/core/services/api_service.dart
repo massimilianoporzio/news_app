9bb725f81dae24d56bdb33158abfaa2e5986ffdb
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-01-15 18:01:28 +0100

    dio vs http

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
+import 'package:news_app/core/services_locator.dart';
 
 //* QUESTO SERVIZIO RESITUISCE IL JSON COME MAP<String,dynamic>
 abstract class ApiService {
@@ -16,6 +18,21 @@ class ApiServiceImpl implements ApiService {
         await http.get(Uri.parse(url).replace(queryParameters: queryParams));
     if (response.statusCode == 200) {
       return json.decode(response.body) as Map<String, dynamic>;
+    } else {
+      //*non ho trovato i dati
+      throw Exception("Status code: ${response.statusCode}");
+    }
+  }
+}
+
+class ApiServiceDioImpl implements ApiService {
+  final Dio dio = sl<Dio>();
+  @override
+  Future<Map<String, dynamic>> getData(
+      {required String url, required Map<String, dynamic> queryParams}) async {
+    final Response response = await dio.get(url, queryParameters: queryParams);
+    if (response.statusCode == 200) {
+      return (response.data) as Map<String, dynamic>;
     } else {
       //*non ho trovato i dati
       throw Exception("Status code: ${response.statusCode}");
--- lib/core/services_locator.dart
+++ lib/core/services_locator.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:news_app/core/services/api_service.dart';
 import 'package:news_app/features/show_news/data/datasources/fetch_from_remote_ds.dart';
@@ -12,6 +13,9 @@ final GetIt sl = GetIt.instance;
 //* anche i USE CASEs
 //*li registro come apiservice ma l'istanza che ci metto è l'implmentazione
 void setupServices() {
+  sl.registerSingleton<Dio>(Dio());
+  //*DIO PRIMA PERCHE' APISERVICE LO USERA'
+  // sl.registerSingleton<ApiService>(ApiServiceDioImpl());
   sl.registerSingleton<ApiService>(ApiServiceImpl());
   sl.registerSingleton<FetchRepo>(FetchRepoImpl());
   sl.registerSingleton<FetchFromRemoteDS>(FetchFromRemoteDSImpl());
